5d525e04dac4e9c3fb3a9d48b5f718c51688a7ec
wn300 <wilmer.mancera93@gmail.com>
2018-01-13 14:05:12 -0500

    add rates services and api, and build of project

--- CORE/Services/Rates/RatesService.cs
+++ CORE/Services/Rates/RatesService.cs
@@ -1,6 +1,7 @@
 ﻿using CORE.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,58 @@ namespace CORE.Services.Rates
                     var vehicleResult = context.Rates.ToList();
 
                     return vehicleResult;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void PostRates(Model.Rates rate)
+        {
+            try
+            {
+                using (var context = DBParkingHRSEntities.Instance)
+                {
+                    context.Rates.Add(rate);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void PutRates(Model.Rates rate)
+        {
+            try
+            {
+                using (var context = DBParkingHRSEntities.Instance)
+                {
+                    context.Entry(rate).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void DeleteRate(int id)
+        {
+            try
+            {
+                using (var context = DBParkingHRSEntities.Instance)
+                {
+                    var itemToRemove = context.Rates.SingleOrDefault(x => x.Id == id);
+                    if (itemToRemove != null)
+                    {
+                        context.Rates.Remove(itemToRemove);
+                        context.SaveChanges();
+                    }
                 }
             }
             catch (Exception ex)
---
+++ RestApi/Controllers/Rates/RatesController.cs
@@ -0,0 +1,44 @@
+﻿using CORE.Services.Rates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace RestApi.Controllers.Rates
+{
+    [RoutePrefix("api")]
+    public class RatesController : ApiController
+    {
+        [HttpGet]
+        [Route("Rates/all")]
+        public object GetAllRatesController()
+        {
+            return Ok(RatesService.GetAllRates());
+        }
+
+        [HttpPost]
+        [Route("Rates/new")]
+        public IHttpActionResult PostRatesController(CORE.Model.Rates rate)
+        {
+            RatesService.PostRates(rate);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("Rates/update")]
+        public IHttpActionResult PutRatesController([FromBody]CORE.Model.Rates rate)
+        {
+            RatesService.PutRates(rate);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("Rates/id/{id:int}")]
+        public IHttpActionResult DeleteRateController(int id)
+        {
+            RatesService.DeleteRate(id);
+            return Ok();
+        }
+    }
+}
